61641a6d5b87c103144a0f0cc9e4ee438498e299
parthmangrola <parthmangrola@gmail.com>
2019-11-03 14:36:51 -0800

    nothing works

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+from heapq import heapify, heappush, heappop
+from copy import deepcopy
 
 goal_state = [[1, 2, 3],
               [4, 5, 6],
@@ -29,20 +30,32 @@ impossible_case = [[1, 2, 3],
                    [8, 7, 0]]
 
 
+class Node:
+    def __init__(self, puzzle, heuristic, depth):
+        self.h = heuristic
+        self.g = depth
+        self.f = heuristic + depth
+        self.puzzle = puzzle
+
+    def __cmp__(self, other):
+        return cmp(self.cost, other.cost)
+
+
 def algorithm_chooser(puzzle):
 
     choose_algorithm = input("Enter your choice of algorithm:" '\n'
-                            "1. Uniform Cost Search" '\n'
-                            "2. A* with Misplaced Tile heuristic" '\n'
-                            "3. A* with the Manhattan distance heuristic" '\n')
+                             "1. Uniform Cost Search" '\n'
+                             "2. A* with Misplaced Tile heuristic" '\n'
+                             "3. A* with the Manhattan distance heuristic" '\n')
     if choose_algorithm == "1":
-        heuristic = 0
+        h = 1
+        search(puzzle, h)
     if choose_algorithm == "2":
-        heuristic = misplaced_tile(puzzle)
-        print("Misplaced tiles: " + str(heuristic) + '\n')
+        h = 2
+        search(puzzle, h)
     if choose_algorithm == "3":
-        heuristic = manhattan_distance(puzzle)
-        print("Manhattan distance is: " + str(heuristic) + '\n')
+        h = 3
+        search(puzzle, h)
 
 
 # Compare puzzle with goal and count the diff
@@ -54,20 +67,128 @@ def misplaced_tile(puzzle):
                 h += 1
     return h
 
-#Compare position of puzzle_num to goal_num and calculate distance
+# Compare position of puzzle_num to goal_num and calculate distance
 #Sum is manhattan_distance
+
+
 def manhattan_distance(puzzle):
     h = 0
     for i in range(3):
         for j in range(3):
-            puzzle_num = puzzle[i][j]
-            goal_num = goal_state[i][j]
-
-            x = abs((puzzle_num / 3) - (goal_num / 3))
-            y = abs((puzzle_num % 3) - (goal_num % 3))
-
-            h += (x + y)
+            h += (abs((puzzle[i][j] // 3) - (goal_state[i][j] // 3)
+                      ) + abs((puzzle[i][j] % 3) - (goal_state[i][j] % 3)))
+    print(h)
     return int(h)
+
+
+def print_puzzle(puzzle):
+    for i in range(3):
+        print(puzzle[i])
+    print('\n')
+
+
+def search(puzzle, h):
+    open_list = []
+    total_nodes_expanded = 0
+    max_nodes_queue = 0
+    depth_goal = 0
+    solved = 0
+
+    if h == 1:
+        heuristic = 0
+    if h == 2:
+        heuristic = misplaced_tile(puzzle)
+    if h == 3:
+        heuristic = manhattan_distance(puzzle)
+
+    start_node = Node(puzzle, heuristic, 0)
+    heappush(open_list, start_node)
+
+    while(open_list):
+
+        heapify(open_list)
+        tmp = heappop(open_list)
+        #print("Best state to expand g(n) = " + str(tmp.g) + " h(n) = " + str(tmp.h) + '\n')
+        if(is_solved(tmp.puzzle)):  # check if goal else expand
+            solved = 1
+            break
+
+        print("Expanding state")
+        total_nodes_expanded += 1
+
+        new_list, total_nodes_expanded = move_tiles(tmp, total_nodes_expanded)
+
+        # update nodes
+
+        for i in range(len(new_list)):
+            if h == 1:
+                new_list[i] = 0
+            if h == 2:
+                new_list[i].h = misplaced_tile(new_list[i].puzzle)
+                new_list[i].f = new_list[i].g + new_list[i].h
+            if h == 3:
+                new_list[i].h = manhattan_distance(new_list[i].puzzle)
+                new_list[i].f = new_list[i].g + new_list[i].h
+            heappush(open_list, new_list[i])
+    if(solved):
+        print("Solved!" + '\n')
+        print_puzzle(tmp.puzzle)
+
+        print('Nodes expanded: ' + str(total_nodes_expanded) + '\n')
+        print('Depth goal: ' + str(tmp.g) + '\n')
+
+def is_solved(puzzle):
+    if(puzzle == goal_state):
+        return 1
+    else:
+        return 0
+
+# pass is current node and total_nodes_expanded to update
+def move_tiles(n, total_nodes_expanded):
+    # find 0 so we know where we can move
+
+    for i in range(3):
+        for j in range(3):
+            x = i
+            y = j
+
+    temp_list = []
+    if x - 1 >= 0:  # down
+        copy_list = deepcopy(n.puzzle)
+        copy_list[x][y] = copy_list[x - 1][y]
+        copy_list[x - 1][y] = 0
+        new_node = Node(copy_list, 0, n.g + 1)
+        temp_list.append(new_node)
+        total_nodes_expanded += 1
+
+    if x + 1 < 3:  # up
+        copy_list = deepcopy(n.puzzle)
+        copy_list[x][y] = copy_list[x + 1][y]
+        copy_list[x + 1][y] = 0
+        new_node = Node(copy_list, 0, n.g + 1)
+        temp_list.append(new_node)
+        total_nodes_expanded += 1
+
+    if y - 1 >= 0:  # left
+
+        copy_list = deepcopy(n.puzzle)
+        copy_list[x][y] = copy_list[x][y - 1]
+        copy_list[x][y - 1] = 0
+
+        new_node = Node(copy_list, 0, n.g + 1)
+        temp_list.append(new_node)
+        total_nodes_expanded += 1
+
+    if y + 1 < 3:  # right
+        copy_list = deepcopy(n.puzzle)
+        copy_list[x][y] = copy_list[x][y + 1]
+        copy_list[x][y + 1] = 0
+
+        new_node = Node(copy_list, 0, n.depth + 1)
+        temp_list.append(new_node)
+        total_nodes_expanded += 1
+
+    return temp_list, total_nodes_expanded
 
 
 def main():
@@ -77,41 +198,40 @@ def main():
 
     if choose_puzzle == "1":  # default puzzle
         choose_default = input("Choose one of the following:" '\n'
-                              "1. Trivial Puzzle" '\n'
-                              "2. Easy Puzzle" '\n'
-                              "3. Oh Boy Puzzle" '\n'
-                              "4. Very Easy Puzzle" '\n'
-                              "5. Doable Puzzle" '\n'
-                              "6. Impossible Puzzle" '\n')
+                               "1. Trivial Puzzle" '\n'
+                               "2. Easy Puzzle" '\n'
+                               "3. Oh Boy Puzzle" '\n'
+                               "4. Very Easy Puzzle" '\n'
+                               "5. Doable Puzzle" '\n'
+                               "6. Impossible Puzzle" '\n')
         if choose_default == "1":
             print("Trivial selected" '\n')
-            print(trivial_case)
+            print_puzzle(trivial_case)
             algorithm_chooser(trivial_case)
 
         if choose_default == "2":
             print("Easy selected" '\n')
-            print(easy_case)
-            print('\n')
+            print_puzzle(easy_case)
             algorithm_chooser(easy_case)
 
         if choose_default == "3":
             print("Oh Boy selected" '\n')
-            print(ohBoy_case)
+            print_puzzle(ohBoy_case)
             algorithm_chooser(ohBoy_case)
 
         if choose_default == "4":
             print("Very Easy selected" '\n')
-            print(veryEasy_case)
+            print_puzzle(veryEasy_case)
             algorithm_chooser(veryEasy_case)
 
         if choose_default == "5":
             print("Doable selected" '\n')
-            print(doable_case)
+            print_puzzle(doable_case)
             algorithm_chooser(doable_case)
 
         if choose_default == "6":
             print("Impossible selected" '\n')
-            print(impossible_case)
+            print_puzzle(impossible_case)
             algorithm_chooser(impossible_case)
 
     if choose_puzzle == "2":  # custom puzzle
@@ -120,7 +240,8 @@ def main():
 
         row_one = input("Enter the first row, use space between numbers." '\n')
 
-        row_two = input("Enter the second row, use space between numbers." '\n')
+        row_two = input(
+            "Enter the second row, use space between numbers." '\n')
 
         row_three = input(
             "Enter the third row, use space between numbers." '\n')
@@ -131,7 +252,7 @@ def main():
 
         custom_puzzle = [row_one, row_two, row_three]
 
-        print(custom_puzzle)
+        print_puzzle(custom_puzzle)
 
         algorithm_chooser(custom_puzzle)
 
